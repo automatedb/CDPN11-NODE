7328462c2cbf2a294bed8d72137b7b618e7dfebd
automatedb <contact@robots-trading.com>
2017-06-29 09:52:18 +0200

    Initial commit

---
+++ 4-functions/index.js
@@ -0,0 +1,21 @@
+/**
+ * Méthode standard
+ */
+// const add = function(a, b) {
+//     return a + b;
+// };
+
+// const add = (a, b) => { return a + b; };
+
+// const add = (a, b) => a + b;
+// console.log(add(1, 1));
+
+// const multiply = a => a * 2;
+// console.log(multiply(2));
+
+const myFunction = (param1, param2) => {
+    param1 = param1 * 2;
+
+    return param1 + param2;
+};
+
