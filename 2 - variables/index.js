8a0de9d99e5de9120a70fe10e81d2655aef64509
automatedb <contact@robots-trading.com>
2017-06-29 09:33:48 +0200

    Initial commit

---
+++ 2 - variables/index.js
@@ -0,0 +1,15 @@
+/**
+ * Déclaration de variables avec let
+ * @type {number}
+ */
+
+let i = 0;
+
+if(1 == 1) {
+    let a = 2;
+    console.log(a);
+    console.log(i);
+}
+
+console.log(i);
+// console.log(a) // => Produit une erreur
